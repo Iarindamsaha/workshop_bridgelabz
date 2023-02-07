47438bd5e9f12e84631ae767e4dcf508f19ad32e
Arindam Saha <asaha15071998@gmail.com>
2023-02-07 08:52:14 +0530

    Adding Feature to display the board

--- src/tic_tac_toe/Main.java
+++ src/tic_tac_toe/Main.java
@@ -12,6 +12,9 @@ public class Main {
 
         System.out.println("Welcome To The Tic Tac Toe Game");
         System.out.println("-------------------------------");
+        board();
+        choose_letter();
+        displayBoard();
 
     }
     public static void board(){
@@ -25,6 +28,14 @@ public class Main {
         System.out.println("Choose a Letter Between X or O");
         player_input = sc.next().toUpperCase().charAt(0);
         computer_input= (player_input=='X')?'O':'X';
+    }
+
+    public static void displayBoard(){
+        System.out.println(board[1]+"||"+board[2]+"||"+board[3]);
+        System.out.println("-------");
+        System.out.println(board[4]+"||"+board[5]+"||"+board[6]);
+        System.out.println("-------");
+        System.out.println(board[7]+"||"+board[8]+"||"+board[9]);
     }
 
 }
