e76734af9a93cca2a0620f0a782e77aff78b4642
Arindam Saha <asaha15071998@gmail.com>
2023-02-07 08:39:29 +0530

    Creating a empty Board For The Game

--- src/tic_tac_toe/Main.java
+++ src/tic_tac_toe/Main.java
@@ -1,7 +1,18 @@
 package tic_tac_toe;
 
 public class Main {
+
+    static char[] board =new char[10];
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Welcome To The Tic Tac Toe Game");
+        System.out.println("-------------------------------");
+
+    }
+    public static void board(){
+        for (int i =1; i<=9; i++){
+            board[i]=' ';
+        }
     }
 }
