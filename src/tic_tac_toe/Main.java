ec87052aca4ebb761e93f937cc9b98b8e1144c5f
Arindam Saha <asaha15071998@gmail.com>
2023-02-07 08:47:07 +0530

    Adding Choose Letter Feature

--- src/tic_tac_toe/Main.java
+++ src/tic_tac_toe/Main.java
@@ -1,8 +1,12 @@
 package tic_tac_toe;
 
+import java.util.Scanner;
+
 public class Main {
 
     static char[] board =new char[10];
+    static char player_input;
+    static char computer_input;
 
     public static void main(String[] args) {
 
@@ -15,4 +19,12 @@ public class Main {
             board[i]=' ';
         }
     }
+
+    public static void choose_letter(){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Choose a Letter Between X or O");
+        player_input = sc.next().toUpperCase().charAt(0);
+        computer_input= (player_input=='X')?'O':'X';
+    }
+
 }
