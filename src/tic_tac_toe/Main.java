d24c69f4e2608b3560cc064a063011628155ab24
Arindam Saha <asaha15071998@gmail.com>
2023-02-08 09:23:52 +0530

    Welcome Main

--- src/tic_tac_toe/Main.java
+++ src/tic_tac_toe/Main.java
@@ -2,6 +2,8 @@ package tic_tac_toe;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("-----Welcome To Tic Tac Toe Game-----");
+
+        System.out.println("Welcome To The Tic Tac Toe Game");
+        System.out.println("-------------------------------");
     }
 }
